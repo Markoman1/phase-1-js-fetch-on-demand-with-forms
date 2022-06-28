a13ab8b0606b35fa4ebd186a0837dd75c832f31e
Mark <mgalex12@icloud.com>
2022-06-28 10:55:56 -0400

    commit tree 9e40d7ff7140b97e5c109866955eeb009f07f58d

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 const init = () => {
-  
+  const inputForm = document.querySelector('form');
+  inputForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    console.log(event);
+
+  });
 }
+
 
 document.addEventListener('DOMContentLoaded', init);
